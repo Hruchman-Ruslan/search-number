feea3367ec282c90648bc265eff4cd5f9131bd39
Ruslan <Giovingo92@gmail.com>
2024-02-22 22:50:30 +0200

    feat: find largest and smallest number

--- src/app/components/search-number.tsx
+++ src/app/components/search-number.tsx
@@ -2,12 +2,12 @@
 
 import React, { ChangeEvent, useState } from "react";
 
-export interface SearchNumberProps {
-  number?: number;
-}
+export interface SearchNumberProps {}
 
-export default function SearchNumber({ number }: SearchNumberProps) {
+export default function SearchNumber({}: SearchNumberProps) {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [largestNumber, setLargestNumber] = useState<number | null>(null);
+  const [smallestNumber, setSmallestNumber] = useState<number | null>(null);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -19,8 +19,18 @@ export default function SearchNumber({ number }: SearchNumberProps) {
       const reader = new FileReader();
       reader.onload = (e) => {
         if (e.target) {
-          const content = e.target.result;
-          console.log(content);
+          const content = e.target.result as string;
+          const numbers = content.split(/\s+/).map(Number);
+          let maxNumber: number | null = null;
+          let minNumber: number | null = null;
+
+          for (const num of numbers) {
+            if (maxNumber === null || num > maxNumber) maxNumber = num;
+            if (minNumber === null || num < minNumber) minNumber = num;
+          }
+
+          setLargestNumber(maxNumber);
+          setSmallestNumber(minNumber);
         }
       };
       reader.readAsText(selectedFile);
@@ -28,18 +38,38 @@ export default function SearchNumber({ number }: SearchNumberProps) {
   };
 
   return (
-    <>
-      <input
-        type="file"
-        onChange={handleFileChange}
-        className="border-2 border-purple-800 hover:bg-sky-700 p-1 rounded-md mr-2 cursor-pointer"
-      />
-      <button
-        onClick={handleSearch}
-        className="bg-purple-700 hover:bg-sky-700 text-white font-bold py-3 px-8 rounded-md transition duration-500 ease-in-out"
-      >
-        Search
-      </button>
-    </>
+    <div className="flex gap-5 items-center">
+      <div>
+        <input
+          type="file"
+          onChange={handleFileChange}
+          className="border-2 border-purple-800 hover:bg-sky-700 p-1 rounded-md mr-2 cursor-pointer transition duration-500 ease-in-out"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-purple-700 hover:bg-sky-700 text-white font-bold py-3 px-8 rounded-md transition duration-500 ease-in-out"
+        >
+          Search
+        </button>
+      </div>
+
+      {largestNumber !== null && smallestNumber !== null && (
+        <div className="border-2 border-purple-800 hover:bg-sky-700 p-1 rounded-md transition duration-500 ease-in-out">
+          <ul className="flex flex-col gap-2 ">
+            <li>
+              <span className=" text-white font-bold">
+                Largest Number: {largestNumber}
+              </span>
+            </li>
+
+            <li>
+              <span className=" text-white font-bold">
+                Smallest Number: {smallestNumber}
+              </span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
